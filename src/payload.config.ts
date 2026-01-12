494c9ca9715d5bd3bcf97b0ae0ec016d319c5d73
David <79004506+davidtomeyan@users.noreply.github.com>
2026-01-13 02:27:41 +0400

    last updates 4

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,5 @@
 // storage-adapter-import-placeholder
-import { sqliteAdapter } from '@payloadcms/db-sqlite';
+import { postgresAdapter } from '@payloadcms/db-postgres';
 import { lexicalEditor } from '@payloadcms/richtext-lexical';
 import path from 'path';
 import { buildConfig, PayloadRequest } from 'payload';
@@ -77,9 +77,9 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  db: sqliteAdapter({
-    client: {
-      url: process.env.DATABASE_URI || '',
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI || '',
     },
   }),
   sharp,
