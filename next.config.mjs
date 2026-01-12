d14fa5cab787c818aa5075df8f2d8bced05f4a13
David <79004506+davidtomeyan@users.noreply.github.com>
2026-01-13 02:43:06 +0400

    last updates next version

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,33 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+import { withPayload } from '@payloadcms/next/withPayload';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
   async redirects() {
-    return [
-      {
-        source: '/posts',
-        destination: '/posts/1',
-        permanent: true,
-      },
-    ]
+    return [];
   },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
-      '.cjs': ['.cts', '.cjs'],
-      '.js': ['.ts', '.tsx', '.js', '.jsx'],
-      '.mjs': ['.mts', '.mjs'],
-    }
+      '.cjs': [
+        '.cts',
+        '.cjs',
+      ],
+      '.js': [
+        '.ts',
+        '.tsx',
+        '.js',
+        '.jsx',
+      ],
+      '.mjs': [
+        '.mts',
+        '.mjs',
+      ],
+    };
 
-    return webpackConfig
+    return webpackConfig;
   },
-}
+};
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPayload(nextConfig, {
+  devBundleServerPackages: false,
+});
