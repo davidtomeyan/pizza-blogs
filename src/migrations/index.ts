bbe6f58a7fb597922d671a2c671efbeb314228e5
David <79004506+davidtomeyan@users.noreply.github.com>
2026-01-13 01:54:49 +0400

    last updates 3

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251116_203552 from './20251116_203552';
-
-export const migrations = [
-  {
-    up: migration_20251116_203552.up,
-    down: migration_20251116_203552.down,
-    name: '20251116_203552'
-  },
-];
